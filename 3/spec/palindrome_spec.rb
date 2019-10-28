309b8d0f1f4fbb0d124f9f021e2406586c456df0
033 <a1111@ubuntu.ubuntu-domain>
2019-10-28 14:28:17 +0700

    more context for nothing

--- 3/spec/palindrome_spec.rb
+++ 3/spec/palindrome_spec.rb
@@ -4,25 +4,31 @@ require 'rspec'
 require './palindrome.rb'
 
 RSpec.describe Palindrome do
-  describe 'palindrome?' do
+  describe '.palindrome?' do
+    subject { Palindrome.palindrome?(value) }
     context 'when it is palindrome' do
-      it 'empty string' do
-        expect(Palindrome.palindrome?('')).to be true
+      context 'and the string is empty' do
+        let(:value) { '' }
+        it { is_expected.to be_truthy }
       end
-      it 'even number of letters' do
-        expect(Palindrome.palindrome?('teet')).to be true
+      context 'and in the string even numbers of letters' do
+        let(:value) { 'teet' }
+        it { is_expected.to be_truthy }
       end
-      it 'odd number of letters' do
-        expect(Palindrome.palindrome?('teset')).to be true
+      context 'and in the string odd numbers of letters' do
+        let(:value) { 'teset' }
+        it { is_expected.to be_truthy }
       end
     end
 
     context 'when it is not palindrome' do
-      it 'even number of letters' do
-        expect(Palindrome.palindrome?('test')).to be false
+      context 'and in the string even numbers of letters' do
+        let(:value) { 'test' }
+        it { is_expected.to be_falsey }
       end
-      it 'odd number of letters' do
-        expect(Palindrome.palindrome?('tesla')).to be false
+      context 'and in the string odd numbers of letters' do
+        let(:value) { 'tesla' }
+        it { is_expected.to be_falsey }
       end
     end
   end
